b55e07501a58679bde8f0d723ead6178bf64e22b
rifaz shaikh razak <rifazshaikh07@gmail.com>
2026-02-18 23:04:59 +0530

    feat: implement JWT authentication and protected route

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import errorHandler from "./middlewares/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 // Error Handling Middleware
 app.use(errorHandler);
 
