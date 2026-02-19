e587b1af856d681f1877cf308c51c2e613e4681f
rifaz shaikh razak <rifazshaikh07@gmail.com>
2026-02-19 22:26:12 +0530

    'feat(frontend): initialize Vite React

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUser({ token });
+    }
+  }, []);
+
+  const login = (data) => {
+    localStorage.setItem("token", data.token);
+    setUser(data);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
