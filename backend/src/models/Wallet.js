159fc9c6897c6f823bb626f9948dfd15ec12de6a
rifaz shaikh razak <rifazshaikh07@gmail.com>
2026-02-19 06:01:44 +0530

    feat(wallet): create wallet model

---
+++ backend/src/models/Wallet.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const walletSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    balance: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Wallet", walletSchema);
