62c14300a8c823611200c72eaeb75ce22ed26b31
rifaz shaikh razak <rifazshaikh07@gmail.com>
2026-02-19 06:03:46 +0530

    feat(wallet): add ransaction model with audit fields

---
+++ backend/src/models/Transaction.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["DEPOSIT", "WITHDRAW"],
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    balanceAfter: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Transaction", transactionSchema);
